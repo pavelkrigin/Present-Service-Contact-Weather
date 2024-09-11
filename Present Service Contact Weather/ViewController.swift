87aa821186f253475981209ba43e89ce3b7dbc53
Pavel Krigin <pavel.krigin.85@gmail.com>
2024-09-11 09:59:01 +0300

    Initial Commit

---
+++ Present Service Contact Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Present Service Contact Weather
+//
+//  Created by Pavel Krigin on 11.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
